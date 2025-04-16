4e82674ea7d730f6daecdadffc3e799921d3f6ce
Wojtas1021 <porazinski.w@gmail.com>
2025-04-16 22:03:45 +0200

    Project dependency and  dependencyInjection classes for projects

---
+++ src/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Ordering.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastuctureServices
+        (this IServiceCollection services, IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("Databaase");
+
+        // add services to the container
+        // services.AddDbContext<OrderContext>(options =>
+        //     options.UseSqlServer(connectionString));
+
+        // services.AddScoped<IApplicationDbContext, IApplicationDbContext>();
+
+        return services;
+    }
+}
