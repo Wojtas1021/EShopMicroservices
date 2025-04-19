d41e20aca59cadf01c780ad202ecfa7c3c9d7621
Wojtas1021 <porazinski.w@gmail.com>
2025-04-19 17:02:05 +0200

    rich value object implementation

--- src/Services/Ordering/Ordering.Domain/ValueObjects/Payment.cs
+++ src/Services/Ordering/Ordering.Domain/ValueObjects/Payment.cs
@@ -7,5 +7,26 @@
         public string Expiration { get; } = default!;
         public string Cvv { get; } = default!;
         public int PaymentMethod { get; } = default!;
+
+        protected Payment()
+        {
+        }
+        private Payment(string? cardName, string cardNumber, string expiration, string cvv, int paymentMethod)
+        {
+            CardName = cardName;
+            CardNumber = cardNumber;
+            Expiration = expiration;
+            Cvv = cvv;
+            PaymentMethod = paymentMethod;
+        }
+        public static Payment Of(string? cardName, string cardNumber, string expiration, string cvv, int paymentMethod)
+        {
+            ArgumentException.ThrowIfNullOrWhiteSpace(cardNumber);
+            ArgumentException.ThrowIfNullOrWhiteSpace(expiration);
+            ArgumentException.ThrowIfNullOrWhiteSpace(cvv);
+            ArgumentOutOfRangeException.ThrowIfGreaterThan(cvv.Length, 3);
+
+            return new Payment(cardName, cardNumber, expiration, cvv, paymentMethod);
+        }
     }
 }
