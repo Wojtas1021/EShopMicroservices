8b8c996e7e003e713c8c9cc673bdf0e68f9b95ed
Wojtas1021 <porazinski.w@gmail.com>
2025-04-29 21:51:17 +0200

    IApplicationDbContext, register, implement

---
+++ src/Services/Ordering/Ordering.Application/Data/IApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿namespace Ordering.Application.Data;
+
+public interface IApplicationDbContext
+{
+    DbSet<Order> Orders { get; }
+    DbSet<OrderItem> OrderItems { get; }
+    DbSet<Customer> Customers { get; }
+    DbSet<Product> Products { get; }
+
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+}
