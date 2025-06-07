49096e9b91905d27b83ac13f00a58e2f42184e81
Wojtas1021 <porazinski.w@gmail.com>
2025-06-07 21:36:21 +0200

    layout

--- src/WebApps/ShoppingWeb/GlobalUsing.cs
+++ src/WebApps/ShoppingWeb/GlobalUsing.cs
@@ -1,3 +1,5 @@
 ﻿global using ShoppingWeb.Models.Catalog;
+global using ShoppingWeb.Models.Basket;
+global using ShoppingWeb.Models.Ordering;
 global using Refit;
 global using ShoppingWeb.Services;
