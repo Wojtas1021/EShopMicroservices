70e95d83e0409e15e11b8c60e27c8fdd39e55a1c
Wojtas1021 <porazinski.w@gmail.com>
2025-06-12 21:58:04 +0200

    service register

--- src/WebApps/ShoppingWeb/GlobalUsing.cs
+++ src/WebApps/ShoppingWeb/GlobalUsing.cs
@@ -3,3 +3,5 @@ global using ShoppingWeb.Models.Basket;
 global using ShoppingWeb.Models.Ordering;
 global using Refit;
 global using ShoppingWeb.Services;
+global using Microsoft.AspNetCore.Mvc;
+global using Microsoft.AspNetCore.Mvc.RazorPages;
--- src/WebApps/ShoppingWeb/Services/IBasketService.cs
+++ src/WebApps/ShoppingWeb/Services/IBasketService.cs
@@ -6,11 +6,33 @@ public interface IBasketService
     Task<GetBasketResponse> GetBasket(string userName);
     
     [Post("/basket-service/basket")]
-    Task<StoreBasketResponse> UpdateBasket(StoreBasketRequest request);
+    Task<StoreBasketResponse> StoreBasket(StoreBasketRequest request);
     
     [Delete("/basket-service/basket/{userName}")]
     Task<DeleteBasketResponse> DeleteBasket(string userName);
     
     [Post("/basket-service/basket/checkout")]
     Task<CheckoutBasketResponse> Checkout(CheckoutBasketRequest request);
+
+    public async Task<ShoppingCartModel> LoadUserBasket()
+    {
+        var userName = "swn";
+        ShoppingCartModel basket;
+
+        try
+        {
+            var getBasketResponse = await GetBasket(userName);
+            basket = getBasketResponse.Cart;
+        }
+        catch (Exception)
+        {
+            basket = new ShoppingCartModel
+            {
+                UserName = userName,
+                Items = []
+            };
+        }
+
+        return basket;
+    }
 }
