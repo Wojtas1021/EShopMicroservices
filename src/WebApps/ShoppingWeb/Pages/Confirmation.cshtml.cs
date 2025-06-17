5cbf82536815c35e2c8ae5e7f481e48d91b2a34a
Wojtas1021 <porazinski.w@gmail.com>
2025-06-17 22:15:48 +0200

    checkout and confirmation razor

---
+++ src/WebApps/ShoppingWeb/Pages/Confirmation.cshtml.cs
@@ -0,0 +1,15 @@
+namespace ShoppingWeb.Pages
+{
+    public class ConfirmationModel : PageModel
+    {
+        public string Message { get; set; } = default!;
+        public void OnGetContact()
+        {
+            Message = "Your email was send";
+        }
+        public void OnGetOrderSubmitted()
+        {
+            Message = "Your order was submitted successfully.";
+        }
+    }
+}
